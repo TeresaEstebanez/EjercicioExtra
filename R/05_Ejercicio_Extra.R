c0cfd3c7482b65b359e970569e0e6793cb245393
TeresaEstebanez <48106791+TeresaEstebanez@users.noreply.github.com>
2019-02-28 23:44:03 +0100

    Update 05_Ejercicio_Extra.R

--- R/05_Ejercicio_Extra.R
+++ R/05_Ejercicio_Extra.R
@@ -1,9 +1,34 @@
+#' Suma
+#'
+#' Esta funcion suma dos numeros.
+#' @param numero_1 numero que se quiere sumar.
+#' @param numero_2 numero que se quiere sumar.
+#' @keywords suma
+#' @export
+
 suma.dos.numeros <- function(numero_1,numero_2){
   return (numero_1+numero_2)
 }
+
+#' Potencia
+#'
+#' Esta funcion devuelve la potencia de dos de un numero.
+#' @param numero numero que se quiere elevar a dos.
+#' @keywords potencia
+#' @export
+
 potencia.dos <- function(numero){
   return (numero*numero)
 }
+
+#' Multiplicacion
+#'
+#' Esta funcion multiplica dos numeros.
+#' @param numero_1 numero que se quiere multiplicar.
+#' @param numero_2 numero que se quiere multiplicar.
+#' @keywords multiplicacion
+#' @export
+
 multiplicacion.dos.numeros <- function(numero_1,numero_2){
   return (numero_1*numero_2)
 }
