c6a8de1ffe8ab3d1d9e023263f06087f8cf80c17
TeresaEstebanez <48106791+TeresaEstebanez@users.noreply.github.com>
2019-02-28 23:36:55 +0100

    Add files via upload

---
+++ R/05_Ejercicio_Extra.R
@@ -0,0 +1,9 @@
+suma.dos.numeros <- function(numero_1,numero_2){
+  return (numero_1+numero_2)
+}
+potencia.dos <- function(numero){
+  return (numero*numero)
+}
+multiplicacion.dos.numeros <- function(numero_1,numero_2){
+  return (numero_1*numero_2)
+}
